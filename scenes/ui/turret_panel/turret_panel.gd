444abf6f5f1311ec2d56ae49705ada2dbc0aadd9
Vicent Valls <vizzent@gmail.com>
2024-10-29 17:01:24 +0100

    Add a way to enable the tower with Events

--- scenes/ui/turret_panel/turret_panel.gd
+++ scenes/ui/turret_panel/turret_panel.gd
@@ -1,7 +1,17 @@
 extends PanelContainer
 
+func _ready() -> void:
+	EventBus.turret_type_unblocked.connect(unblock_turret_type)
+
 func _on_bow_turret_button_pressed() -> void:
 	EventBus.send_turret_type_selected(AbstractTurret.Type.BOW)
 
 func _on_bomb_turret_button_pressed() -> void:
 	EventBus.send_turret_type_selected(AbstractTurret.Type.BOMB)
+
+func unblock_turret_type(turret_type : AbstractTurret.Type) -> void:
+	match turret_type:
+		0:
+			$MarginContainer/GridContainer/BowTurretButton.disabled = false
+		1:
+			$MarginContainer/GridContainer/BombTurretButton.disabled = false
