79801ee14413b6de726e959b58ecafacf034b206
Vicent Valls <vizzent@gmail.com>
2024-11-03 08:00:41 +0100

    Add changes of music

--- scripts/music/music_controller.gd
+++ scripts/music/music_controller.gd
@@ -1,4 +1,10 @@
 extends AudioStreamPlayer
 
-func swith_to_tutorial():
+func _ready() -> void:
+	EventBus.level_finished.connect(switch_to_intro)
+
+func switch_to_tutorial():
 	self["parameters/switch_to_clip"] = "Tutorial"
+
+func switch_to_intro():
+	self["parameters/switch_to_clip"] = "Intro"
