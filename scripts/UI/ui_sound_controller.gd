d262ac06cb5eceeea21efa42cc2e2381b5dde8dd
Vicent Valls <vizzent@gmail.com>
2024-11-03 07:52:08 +0100

    Add new sounds to the UI

--- scripts/UI/ui_sound_controller.gd
+++ scripts/UI/ui_sound_controller.gd
@@ -4,8 +4,12 @@ extends AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	stop()
-	EventBus.round_finished.connect(success)
+	EventBus.round_finished.connect(end_round)
 
 func success() -> void:
 	self["parameters/switch_to_clip"] = "success"
+	play()
+
+func end_round() -> void:
+	self["parameters/switch_to_clip"] = "end_round"
 	play()
